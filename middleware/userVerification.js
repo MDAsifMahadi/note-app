5272922b5c0e2ca93ba43aadafae956fff72424e
MD Asif Mahadi <153858628+MDAsifMahadi@users.noreply.github.com>
2024-09-20 16:36:39 +0100

    n

--- middleware/userVerification.js
+++ middleware/userVerification.js
@@ -5,7 +5,9 @@ const { User }= require("../models/model");
 const userVerification = async (req, res, next) => {
     try {
         const token = req.cookies.token;
-        
+
+        console.log(token);
+
         if(!token) {
             res.status(303).json({
                 message  : "please login"
